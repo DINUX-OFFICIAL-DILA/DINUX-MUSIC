df5873ba8c30d37597300edb56a9209d4cab5850
DINUX-OFFICIAL-DILA <122338374+DINUX-OFFICIAL-DILA@users.noreply.github.com>
2024-12-16 01:54:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || “KTIURZyL#y0dN3oVdDkDZgRXqUwEet0PLXrV3YH-EcfoOZdNDXHk”,
+SESSION_ID: process.env.SESSION_ID || "KTIURZyL#y0dN3oVdDkDZgRXqUwEet0PLXrV3YH-EcfoOZdNDXHk"
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/KWSKzft/CE76-C7-C8-EAAF-4-D05-B6-B0-475987-FE321-E.jpg",
 ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now",
 };
